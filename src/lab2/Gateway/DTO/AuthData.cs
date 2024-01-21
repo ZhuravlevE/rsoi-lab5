c07060946f720e2bebdc7a3f20bcc5566f1ccd79
ZhuravlevE <vip.zhyra@mail.ru>
2024-01-21 16:44:05 +0300

    Add src

---
+++ src/lab2/Gateway/DTO/AuthData.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Gateway.DTO
+{
+    [DataContract]
+    public class AuthData
+    {
+        [DataMember(Name = "access_token")]
+        public string AccessToken { get; set; }
+
+        public AuthData(string accessToken)
+        {
+            AccessToken = accessToken;
+        }
+    }
+}
---
+++ src/lab2/Gateway/DTO/LoginCredential.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization;
+
+namespace Gateway.DTO
+{
+    [DataContract]
+    public class LoginCredential
+    {
+        [DataMember(Name = "grant_type")]
+        public string GrantType { get; set; }
+
+        [DataMember(Name = "username")]
+        public string Username { get; set; }
+
+        [DataMember(Name = "password")]
+        public string Password { get; set; }
+
+        [DataMember(Name = "client_id")]
+        public string ClientId { get; set; }
+
+        [DataMember(Name = "client_secret")]
+        public string ClientSecret { get; set; }
+
+        public LoginCredential(string grantType,
+            string username,
+            string password,
+            string clientId,
+            string clientSecret)
+        {
+            GrantType = grantType;
+            Username = username;
+            Password = password;
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+        }
+    }
+}
